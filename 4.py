5715dd2fd9df219e1a7842f34c0834b32e814e5b
Akram <akramkazmi71@gmail.com>
2021-10-02 17:49:38 +0530

    modified code

--- 4.py
+++ 4.py
@@ -2,8 +2,8 @@ list1 = list(map(int, input().split()))
 even = 0
 odd = 0
 for i in list1:
-    if i % 2 == 0:
-        even += 1
-    else:
+    if i % 2 != 0:
         odd += 1
+    else:
+        even += 1
 print('Even=',even,', odd=',odd)
